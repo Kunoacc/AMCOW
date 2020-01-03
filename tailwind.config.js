0a71dbda67809ec1bde2dcc7b9355f8c27308915
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2020-01-03 17:26:47 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {},
+    fontFamily: {
+      'display': ['"DM Serif Text"', 'serif'],
+      'body': ['"Poppins"', 'serif']
+    },
+  },
+  variants: {},
+  plugins: []
+}
